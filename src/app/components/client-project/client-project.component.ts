bfe359408454d7cf33f8a6e7c664e86a916b79c7
Maddineni Pujitha <marrisaichandu143@gmail.com>
2024-11-30 12:32:37 +0530

    Completed With Reactive Forms and It's Validation

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ClientService } from '../../services/client.service';
+import { APIResponseModel, IEmployee } from '../../model/interface/role';
+import { Client } from '../../model/classes/client';
 
 @Component({
   selector: 'app-client-project',
@@ -9,24 +12,57 @@ import { RouterLink } from '@angular/router';
   templateUrl: './client-project.component.html',
   styleUrl: './client-project.component.css'
 })
-export class ClientProjectComponent {
-  clientList: any;
-  clientObj: any;
+export class ClientProjectComponent implements OnInit{
 
   projectForm:FormGroup=new FormGroup({
-    clientProjectId:new FormControl(0),
-    projectName:new FormControl(''),
-    startDate:new FormControl(''),
-    expectedEndDate:new FormControl(''),
-    leadByEmpId:new FormControl(''),
-    completedDate:new FormControl(''),
-    contactPerson:new FormControl(''),
-    contactPersonContactNo:new FormControl(''),
-    totalEmpWorking:new FormControl(''),
-    projectCost:new FormControl(''),
-    projectDetails:new FormControl(''),
-    contactPersonEmailId:new FormControl(''),
+    clientProjectId:new FormControl(0,[Validators.required]),
+    projectName:new FormControl('',[Validators.required,Validators.minLength(4)]),
+    startDate:new FormControl('',[Validators.required]),
+    expectedEndDate:new FormControl('',[Validators.required]),
+    leadByEmpId:new FormControl('',[Validators.required]),
+    completedDate:new FormControl('',[Validators.required]),
+    contactPerson:new FormControl('',[Validators.required]),
+    contactPersonContactNo:new FormControl('',[Validators.required]),
+    totalEmpWorking:new FormControl('',[Validators.required]),
+    projectCost:new FormControl('',[Validators.required]),
+    projectDetails:new FormControl('',[Validators.required]),
+    contactPersonEmailId:new FormControl('',[Validators.required,Validators.email]),
     clientId:new FormControl('')
   })
+
+  clientService=inject(ClientService);
+
+  employeeList:IEmployee[]=[];
+  clientList: Client[]=[];
+
+  ngOnInit(): void {
+      this.getAllEmployee();
+      this.getAllClients();
+  }
+
+  getAllEmployee(){
+    this.clientService.getAllEmployees().subscribe((res:APIResponseModel)=>{
+      this.employeeList=res.data;
+    })
+  }
+
+  getAllClients(){
+    this.clientService.getAllClients().subscribe((res:APIResponseModel)=>{
+      this.clientList=res.data;
+    })
+  }
+
+  onSaveProject(){
+    const formValue=this.projectForm.value;
+    debugger;
+    this.clientService.addClientProjectUpdate(formValue).subscribe((res:APIResponseModel)=>{
+      if(res.result)
+      {
+        alert("project Created Successfully")
+      }else{
+        alert(res.message)
+      }
+    })
+  }
 
 }
--- src/app/model/interface/role.ts
+++ src/app/model/interface/role.ts
@@ -1,15 +1,24 @@
-export interface IRole{
-    roleId:number;
-    role:string;
+export interface IRole {
+    roleId: number;
+    role: string;
 }
 
-export interface IDesignation{
-    designationId:number;
-    designation:string;
+export interface IDesignation {
+    designationId: number;
+    designation: string;
 }
 
-export interface APIResponseModel{
-    message:string;
-    result:boolean;
-    data:any;
+export interface APIResponseModel {
+    message: string;
+    result: boolean;
+    data: any;
+}
+
+export interface IEmployee {
+    empName: string;
+    empId: number;
+    empCode: string;
+    empEmailId: string;
+    empDesignation: string;
+    role: string;
 }
